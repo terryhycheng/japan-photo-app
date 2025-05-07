56b45ece28af35d2414512d753cda471373ec3c1
terryhycheng <terryhycheng@gmail.com>
2025-05-07 22:02:54 +0100

    test: test github workflow

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { message: string } {
-    return { message: 'Hello World! 你好' };
+    return { message: 'Hello World! 你好呀' };
   }
 }
